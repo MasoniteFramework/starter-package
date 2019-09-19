25695914b4940abfedd2e717fb82a191e4e0228c
Joseph Mancuso <idmann509@gmail.com>
2019-09-18 22:13:38 -0400

    cleaned package

--- routes/web.py
+++ routes/web.py
@@ -1,13 +1,6 @@
 """ Web Routes """
 from masonite.routes import Get, Post
 
-from src.api.routes import JWTRoutes, TokenRoutes
-from app.resources.UserResource import UserResource
-
 ROUTES = [
     Get().route('/', 'WelcomeController@show').name('welcome'),
-    UserResource('/api/user').routes(),
-
-    TokenRoutes('/token'),
-    JWTRoutes('/authorize'),
 ]
