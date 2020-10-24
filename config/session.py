d031e756681b9a8f3428b79fd7a2057fe1987303
Samuel Girardin <samuelgirardin@pm.me>
2020-10-24 15:45:57 +0200

    update session config

--- config/session.py
+++ config/session.py
@@ -1,18 +1,18 @@
-""" Session Settings """
+"""Session Settings."""
 
-import os
+from src.masonite import env
 
-"""
-|--------------------------------------------------------------------------
-| Session Driver
-|--------------------------------------------------------------------------
-|
-| Sessions are able to be linked to an individual user and carry data from
-| request to request. The memory driver will store all the session data
-| inside memory which will delete when the server stops running.
-|
-| Supported: 'memory', 'cookie'
-|
+"""Session Driver
+Sessions are able to be linked to an individual user and carry data from
+request to request. The memory driver will store all the session data
+inside memory which will delete when the server stops running.
+Supported: 'memory', 'cookie'
 """
 
-DRIVER = os.getenv("SESSION_DRIVER", "cookie")
+DRIVER = env('SESSION_DRIVER', 'cookie')
+
+DRIVERS = {
+    'cookie': {
+        'flash_expires': '2 seconds'
+    }
+}
