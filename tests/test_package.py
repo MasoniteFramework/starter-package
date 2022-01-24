157cc0ef4bbe1794ae51874f5295be71e4477ba4
Samuel Girardin <samuelgirardin@pm.me>
2022-01-24 10:07:01 +0100

    update

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,10 +1,10 @@
+from masonite.tests import TestCase
 
-# class TestPackage(TestCase):
 
-#     def setUp(self):
-#         super().setUp()
-    
-#     def test_can_get_home_route(self):
-#         self.assertTrue(
-#             self.get('/home').contains('Hello World')
-#         )
+class TestPackage(TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def test_can_get_home_route(self):
+        self.assertTrue()
